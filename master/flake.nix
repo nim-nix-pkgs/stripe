6ac9d8647df235257cc850ec19f4ee5d59965978
hugosenari <hugosenari@gmail.com>
2022-03-12 15:28:12 -0300

    chore: re index stripe

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim client for Stripe.com'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-stripe-master.flake = false;
+  inputs.src-stripe-master.owner = "iffy";
+  inputs.src-stripe-master.ref   = "refs/heads/master";
+  inputs.src-stripe-master.repo  = "nim-stripe";
+  inputs.src-stripe-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-stripe-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-stripe-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
